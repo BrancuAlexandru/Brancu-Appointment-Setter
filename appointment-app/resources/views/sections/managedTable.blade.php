efee7b27f7a3ba1b1ea7f3a6e83d44ab69775f06
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-07-04 13:34:25 +0300

    Made app more modular by using @include

---
+++ appointment-app/resources/views/sections/managedTable.blade.php
@@ -0,0 +1,43 @@
+<form action="" style="max-width: 70%; margin-top: 2px; margin-left: auto; margin-right: auto;">
+  <div class="container text-center d-flex flex-row align-items-start justify-content-center" style="width: 80%; margin: 0 auto;">
+    <div id="column1" class="d-flex flex-column align-items-center justify-content-center" style="background-color: #efefef; max-width: max-content;">
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">Email</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">aaaaaaaaaaaaaaaaaexample1@gmail.com</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">example2@gmail.com</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">example3@gmail.com</p>
+    </div>
+    <div id="column2" class="d-flex flex-column align-items-center justify-content-center" style="background-color: #efefef; max-width: max-content;">
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">Month</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">July</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">July</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">August</p>
+    </div>
+    <div id="column2" class="d-flex flex-column align-items-center justify-content-center" style="background-color: #efefef; max-width: max-content;">
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">Day</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">25</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">25</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">1</p>
+    </div>
+    <div id="column3" class="d-flex flex-column align-items-center justify-content-center" style="background-color: #efefef; max-width: max-content;">
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">Time</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">9:00-10:00</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">10:30-11:30</p>
+      <p style="margin-bottom: 0; padding: 10px 20px; border-right: 2px solid white; width: 100%;">15:30-16:30</p>
+    </div>
+    <div id="column4" class="d-flex flex-column align-items-center justify-content-center" style="background-color: #efefef; max-width: max-content;">
+      <p style="margin-bottom: 0; padding: 10px 20px; width: 100%;">Actions</p>
+      <div style="margin-bottom: 0; width: 100%; margin-left: 0.125rem; margin-right: 0.125rem;">
+        <button class="btn btn-primary m-1" style="padding-top: 0.31rem; padding-bottom: 0.31rem;">Edit</button>
+        <button class="btn btn-danger m-1" style="padding-top: 0.31rem; padding-bottom: 0.31rem;">Delete</button>
+      </div>
+      <div style="margin-bottom: 0; width: 100%; margin-left: 0.125rem; margin-right: 0.125rem;">
+        <button class="btn btn-primary m-1" style="padding-top: 0.31rem; padding-bottom: 0.31rem;">Edit</button>
+        <button class="btn btn-danger m-1" style="padding-top: 0.31rem; padding-bottom: 0.31rem;">Delete</button>
+      </div>
+      <div style="margin-bottom: 0; width: 100%; margin-left: 0.125rem; margin-right: 0.125rem;">
+        <button class="btn btn-primary m-1" style="padding-top: 0.31rem; padding-bottom: 0.31rem;">Edit</button>
+        <button class="btn btn-danger m-1" style="padding-top: 0.31rem; padding-bottom: 0.31rem;">Delete</button>
+      </div>
+    </div>
+  </div>
+</form>
